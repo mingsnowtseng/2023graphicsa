9780ae24119beaf199b945c5965c4cceb81a420d
mingsnowtseng <aa503164@gmail.com>
2023-04-26 12:05:14 +0800

    week11

---
+++ week11_keyboard/main.cpp
@@ -0,0 +1,22 @@
+#include <GL/glut.h>
+void display()
+{
+    glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    if(key==27) exit(0); ///27表示Esc鍵, 這句表達按下Esc鍵就離開
+}
+int main(int argc, char**argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week11");
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+}
